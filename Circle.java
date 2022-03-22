d13894ab31371d5d414c4b6c4edb8adb8ab49baa
SemirHajdar <98660616+SemirHajdar@users.noreply.github.com>
2022-03-22 18:31:45 +0100

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -10,7 +10,7 @@ public class Circle extends Shape {
         this.radius=radius;
     }
     @Override
-    protected double Circumference () {
+    protected double circumference () {
         circumference = 2*radius*PI;
         if (circumference<0)
             throw new IllegalArgumentException("Circumference of Circle is less then 0, please check your Input!!");
@@ -18,11 +18,10 @@ public class Circle extends Shape {
         }
 
     @Override
-    protected double Area () {
+    protected double area () {
         area = PI*radius*radius;
         if (area<0)
             throw new IllegalArgumentException("Area of Circle is less then 0, please check your Input!!");
         return area;
         }
     }
-
